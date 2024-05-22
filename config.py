d66909b70d54a07e4a3962d3f3d09e7b623aa62b
Khanarm <142902201+Khanarm@users.noreply.github.com>
2024-05-22 13:41:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,13 +11,14 @@ class Config(object):
     BOT_TOKEN = os.environ.get('BOT_TOKEN', '') # ⚠️ Required
 
     # database config
-    DB_URL  = os.environ.get("DB_URL","") # ⚠️ Required
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://bikash:bikash@bikash.3jkvhp7.mongodb.net/?retryWrites=true&w=majority
+    ") # ⚠️ Required
 
     # Other Config 
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "") # ⚠️ Required
     BOT_UPTIME  = time.time()
-    OWNER = int(os.environ.get('OWNER', '')) # ⚠️ Required
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "")) # ⚠️ Required
+    OWNER = int(os.environ.get('OWNER', '6049507444')) # ⚠️ Required
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002038655597")) # ⚠️ Required
     APPROVED_WELCOME_TEXT = os.environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYou're Auto Approved. ✅")
     LEAVING_BY_TEXT = os.environ.get("APPROVED_WELCOME_TEXT", "👋 Bye {mention} !\nSee You Soon by {title}\n\nYou Left. ⛔")
     FORCE_SUB = os.environ.get('FORCE_SUB', '') # ⚠️ Required
